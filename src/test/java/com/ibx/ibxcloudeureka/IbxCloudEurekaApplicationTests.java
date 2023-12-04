d8f25e37d2a5d160be8f1224d4e5115efa133a7f
Xavi Iborra <xavi.iborra.95@gmail.com>
2023-12-04 20:13:05 +0100

    Create microservice

---
+++ src/test/java/com/ibx/ibxcloudeureka/IbxCloudEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibx.ibxcloudeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IbxCloudEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
